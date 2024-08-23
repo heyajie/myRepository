baa3ff39f3a4803d23fe3d88c9dee73116b18242
heyajie <hehe_teacher@163.com>
2024-08-23 17:24:22 +0800

    one time

--- pigfile/src/main/java/com/pig/utils/PigTimeUtil.java
+++ pigfile/src/main/java/com/pig/utils/PigTimeUtil.java
@@ -2,6 +2,7 @@ package com.pig.utils;
 
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Logger;
 
@@ -14,12 +15,22 @@ public class PigTimeUtil {
         return dateFormat.format(new Date());
     }
 
+    public static String getNowWeekNum(){
+        Calendar calendar = Calendar.getInstance();
+        // 设置星期一为一周开始的第一天（‌可选，‌根据需要设置）‌
+        calendar.setFirstDayOfWeek(Calendar.MONDAY);
+        // 获得当前日期属于今年的第几周
+        int weekOfYear = calendar.get(Calendar.WEEK_OF_YEAR);
+        return ""+weekOfYear;
+    }
 
 
     public static void main(String[] args) {
 
         // //Logger log = Logger.getLogger(PigTimeUtil.class.getName());
         log.info(getToday());
+
+        log.info(getNowWeekNum());
     }
 
 }
