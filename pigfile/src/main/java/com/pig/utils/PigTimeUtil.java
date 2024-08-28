032e5b5889cd50c04af20a992be3edeef4993953
heyajie <hehe_teacher@163.com>
2024-08-28 17:22:25 +0800

    week id

--- pigfile/src/main/java/com/pig/utils/PigTimeUtil.java
+++ pigfile/src/main/java/com/pig/utils/PigTimeUtil.java
@@ -1,6 +1,7 @@
 package com.pig.utils;
 
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -22,6 +23,35 @@ public class PigTimeUtil {
         // 获得当前日期属于今年的第几周
         int weekOfYear = calendar.get(Calendar.WEEK_OF_YEAR);
         return ""+weekOfYear;
+    }
+
+    /**
+     * get week id by the day
+     *
+     * @param dayTime
+     * @return
+     */
+    public static String getWeekNum(String dayTime){
+        String resKey = "0";
+        Calendar calendar = Calendar.getInstance();
+        //
+        Date dateOne;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        try {
+            dateOne = sdf.parse(dayTime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return resKey;
+        }
+        calendar.setTime(dateOne);
+        // 设置星期一为一周开始的第一天（‌可选，‌根据需要设置）‌
+        calendar.setFirstDayOfWeek(Calendar.MONDAY);
+        // 获得当前日期属于今年的第几周
+        int weekOfYear = calendar.get(Calendar.WEEK_OF_YEAR);
+        if(weekOfYear >=0){
+            resKey = String.valueOf(weekOfYear);
+        }
+        return resKey;
     }
 
 
